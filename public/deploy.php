5e96126a73861c88d179da6f8fe93a7eadd5c2c5
lambq <415113157@qq.com>
2017-06-24 18:52:19 +0800

    lambq for sxzhihui

--- public/deploy.php
+++ public/deploy.php
@@ -7,7 +7,7 @@
  */
 error_reporting( E_ALL );
 
-$handle = popen('cd /home/wwwroot/sxzhihui.rushangkeji.com && git pull 2>&1','r');
+$handle = popen('cd /www/wwwroot/sxzhihui.rushangkeji.com && git pull 2>&1','r');
 $read   = stream_get_contents($handle);
 printf($read);
 pclose($handle);
